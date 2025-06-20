d22050b679920173f06cd49833854c6b796d2e69
疯の葉 <crazyleaf0912@outlook.com>
2025-06-21 01:27:39 +0800

    hlymcn modify edition.

--- Kill.cs
+++ Kill.cs
@@ -1,30 +1,28 @@
 ﻿using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Events;
 
-namespace Preach.CS2.Plugins.Kill;
+namespace Kill;
 
 public class Kill : BasePlugin, IPluginConfig<KillConfig>
 {
     public override string ModuleName => "Kill";
-    public override string ModuleVersion => "0.2.0";
+    public override string ModuleVersion => "[hlymcnmodify]0.5.0";
     public override string ModuleAuthor => "Preach";
 
-    required public KillConfig Config {get; set; }
+    required public KillConfig Config { get; set; }
 
     public override void Load(bool hotReload)
     {
-        AddCommand("suicide", "Kill yourself", (ply, info) => {
-            SuicideHandler(ply, info);
-        });
-        AddCommand("kill", "Kill yourself", (ply, info) => {
-            SuicideHandler(ply, info);
-        });
-
+        AddCommand("kill", "Kill yourself", SuicideHandler);
     }
 
-    private void SuicideHandler(CCSPlayerController ply, CommandInfo info) {
-        if (ply == null || !ply.IsValid || !ply.PawnIsAlive || !ply.PlayerPawn.IsValid)
+    private void SuicideHandler(CCSPlayerController? ply, CommandInfo? info)
+    {
+        if (ply == null || !ply.IsValid || !ply.PawnIsAlive || ply.PlayerPawn == null || !ply.PlayerPawn.IsValid)
             return;
 
         if (!Config.ToggleCommand)
@@ -33,8 +31,25 @@ public class Kill : BasePlugin, IPluginConfig<KillConfig>
             return;
         }
 
-        ply.PlayerPawn.Value.CommitSuicide(true, false);
+        var playerPawn = ply.PlayerPawn.Value;
+        playerPawn?.CommitSuicide(true, false);
     }
+
+    [GameEventHandler(HookMode.Post)]
+    public HookResult OnChat(EventPlayerChat @event, GameEventInfo info)
+    {
+        var player = Utilities.GetPlayerFromUserid(@event.Userid);
+        if (player is not null)
+        {
+            var text = @event.Text.Trim();
+            if (string.Equals(text, "kill", StringComparison.OrdinalIgnoreCase))
+            {
+                SuicideHandler(player, null);
+            }
+        }
+        return HookResult.Continue;
+    }
+
     public void OnConfigParsed(KillConfig config)
     {
         Config = config;
@@ -45,5 +60,4 @@ public class KillConfig : BasePluginConfig
 {
     [JsonPropertyName("Toggle Command")]
     public bool ToggleCommand { get; set; } = true;
-
 }
